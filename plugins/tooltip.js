ed7f487bf2757ab3980b0c8a07211e37e8adf9e3
John Best <44152393+Xilophinum@users.noreply.github.com>
2025-11-01 09:10:30 -0400

    feat: Refactor tooltip functionality by integrating directive directly into the plugin for improved usability

--- plugins/tooltip.js
+++ plugins/tooltip.js
@@ -1,10 +1,48 @@
 /**
- * Tooltip Plugin
- * Globally registers the v-tooltip directive
+ * Tooltip directive plugin
+ * Usage: <span v-tooltip:top="'Tooltip text'">Hover me</span>
+ * Positions: top, bottom, left, right, topleft, topright, bottomleft, bottomright
  */
 
-import tooltipPlugin from '~/composables/useTooltip.js'
-
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(tooltipPlugin)
+  nuxtApp.vueApp.directive('tooltip', {
+    mounted(el, binding) {
+      const tooltipText = binding.value
+      if (!tooltipText) return
+
+      // Store tooltip text in data attribute
+      el.setAttribute('data-tooltip', tooltipText)
+
+      // Add tooltip class
+      el.classList.add('has-tooltip')
+
+      // Handle position argument (top, bottom, left, right)
+      const position = binding.arg || 'top' // default to top
+      el.setAttribute('data-tooltip-position', position)
+    },
+
+    updated(el, binding) {
+      const tooltipText = binding.value
+      if (tooltipText) {
+        el.setAttribute('data-tooltip', tooltipText)
+        if (!el.classList.contains('has-tooltip')) {
+          el.classList.add('has-tooltip')
+        }
+
+        // Update position if it changed
+        const position = binding.arg || 'top'
+        el.setAttribute('data-tooltip-position', position)
+      } else {
+        el.removeAttribute('data-tooltip')
+        el.removeAttribute('data-tooltip-position')
+        el.classList.remove('has-tooltip')
+      }
+    },
+
+    unmounted(el) {
+      el.removeAttribute('data-tooltip')
+      el.removeAttribute('data-tooltip-position')
+      el.classList.remove('has-tooltip')
+    }
+  })
 })
